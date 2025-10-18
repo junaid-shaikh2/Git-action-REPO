04a4ec6085b5bae6814eb3a3f3673ec7bb18ad4c
junaid-shaikh2 <junaid.shaikh.kn@gmail.com>
2025-10-18 14:27:32 +0500

    Change greeting message from Jahanzaib to Junaid

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-  document.getElementById('message').innerText = "Hello, Jahanzaib! This is your GitHub Pages site 😎";
+  document.getElementById('message').innerText = "Hello, Junaid! This is your GitHub Pages site 😎";
 }
